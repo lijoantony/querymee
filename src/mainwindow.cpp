876bba470a84c7408741ac36631e58a79355617c
Reto Zingg <g.d0b3rm4n@gmail.com>
2010-09-21 20:31:11 +0300

    add busy indicator at start up 5

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -31,7 +31,6 @@ MainWindow::MainWindow()
 
 #ifdef Q_WS_MAEMO_5
     this->setAttribute(Qt::WA_Maemo5StackedWindow, true);
-    this->setAttribute(Qt::WA_Maemo5ShowProgressIndicator, true);
 #endif
 
 }
